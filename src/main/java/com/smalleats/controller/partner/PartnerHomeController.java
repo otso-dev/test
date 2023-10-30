b33f3ac45748ec84641dd05f70b49c402eeac408
otso <hhgg0017@gmail.com>
2023-10-30 18:13:07 +0900

    [flow-3301] PR 피드백(트랜잭션, 가독성) 피드백 수정중

--- src/main/java/com/smalleats/controller/partner/PartnerHomeController.java
+++ src/main/java/com/smalleats/controller/partner/PartnerHomeController.java
@@ -33,7 +33,7 @@ public class PartnerHomeController {
     @RequestMapping(value = "/partner/partnerpage",method = RequestMethod.GET)
     public String partnerHome(Model model){
         //임시
-        model.addAttribute("pendingFood",partnerFoodService.getPendingFood());
+//        model.addAttribute("pendingFood",partnerFoodService.getPendingFood());
         return"/partner/partnerpage";
     }
     @RequestMapping(value = "/partner/foodregister", method = RequestMethod.GET)
